fc19dde6c671490ce8d2c0ad228bf8f0cda5749d
jefersonoliveira <adv.jefersonoliveira@gmail.com>
2025-02-18 17:51:10 -0300

    upd: criado módulo, componente e rota da func dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { ToastModule } from 'primeng/toast';
 import { CookieService } from 'ngx-cookie-service';
+import { MessageService } from 'primeng/api';
 
 
 @NgModule({
@@ -31,7 +32,7 @@ import { CookieService } from 'ngx-cookie-service';
     ButtonModule,
     ToastModule //notificações que exibimos ao usuario
   ],
-  providers: [CookieService],
+  providers: [CookieService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
